0ec22a2426fb07b057b898d49cd5a6ab31a667ff
MatAIQUE <Matt.Tan@aiqueinnovation.com>
2023-10-18 11:01:18 +0800

    align the header notif and user function in the design

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -29,98 +29,101 @@ const Layout = () => {
               </div>
 
               {user ? (
-                // <div className='col-6 my-auto'>
-                //   <ul
-                //       style={{
-                //         display: "flex",
-                //         flexDirection: "row",
-                //         justifyContent: "flex-end",
-                //         gap: "20px",
-                //       }}
-                //     >
-                //       <li className='nav-item dropdown'>
-                //         <a
-                //           href='#'
-                //           role='button'
-                //           data-bs-toggle='dropdown'
-                //           aria-expanded='false'
-                //           className=' text-decoration-none text-black'
-                //         >
-                //           <i
-                //             className='bi bi-bell'
-                //             style={{ fontSize: "30px" }}
-                //           ></i>
-                //         </a>
-                //         <ul className='dropdown-menu py-3'>
-                //           {isLoadingTrans && "Loading Transaction ..."}
-                //           {transaction.map((trans) => (
-                //             <li key={trans._id}>
-                //               <a
-                //                 onClick={(e) => handleQuickPin(trans)}
-                //                 className='dropdown-item mb-2'
-                //                 // href='/generate-view-qr'
-                //               >
-                //                 <span className='fw-bold small text-success'>
-                //                   {/* Your Quickpin is {trans.qpin} */}
-                //                   {/* get service name and replace module data */}
-                //                   Your quickpin for your {trans.moduleData} is
-                //                   ready
-                //                 </span>
-                //                 <br />
-                //                 <span className='text-muted small'>
-                //                   Transaction {trans.transNumber}
-                //                 </span>
-                //                 <br />
-                //                 <span className='text-muted  small'>
-                //                   Location: {trans.locName}
-                //                 </span>
-                //                 <br />
-                //                 <span className='text-muted small'>
-                //                   Tap here to view QR
-                //                 </span>
-                //               </a>
-                //             </li>
-                //           ))}
-                //         </ul>
-                //       </li>
-                //       <li className='nav-item dropdown'>
-                //         <a
-                //           href='#'
-                //           role='button'
-                //           data-bs-toggle='dropdown'
-                //           aria-expanded='false'
-                //           className='text-decoration-none text-success'
-                //         >
-                //           <i
-                //             className='bi bi-person-circle'
-                //             style={{ fontSize: "30px" }}
-                //           ></i>
-                //         </a>
-                //         <ul className='dropdown-menu'>
-                //           <li className='pe-none'>
-                //             <a className='dropdown-item py-3'>
-                //               <strong className="text-capitalize">{user.firstName}</strong>
-                //             </a>
-                //           </li>
-                //           <div className='dropdown-divider'></div>
-                //           <li>
-                //             <a
-                //               className='dropdown-item text-center py-3'
-                //               role='button'
-                //               onClick={logout}
-                //             >
-                //               <span className='fw-bold small pe-pointer text-danger'>
-                //                 Logout
-                //               </span>
-                //             </a>
-                //           </li>
-                //         </ul>
-                //       </li>
-                //   </ul>
-                // </div>
-                <div className="col-6 my-auto d-flex justify-content-end">
-                  <ul className="my-auto ps-4">bell</ul>
-                  <ul className="my-auto ps-4">user</ul>
+                <div className='col-6 my-auto d-flex justify-content-end'>
+                  <ul className='my-auto ps-4'>
+                    {" "}
+                    <li className='nav-item dropdown'>
+                      <a
+                        href='#'
+                        role='button'
+                        data-bs-toggle='dropdown'
+                        aria-expanded='false'
+                        className=' text-decoration-none text-black'
+                      >
+                        <i
+                          className='bi bi-bell'
+                          style={{ fontSize: "30px" }}
+                        ></i>
+                      </a>
+                      <ul className='dropdown-menu py-3'>
+                        {isLoadingTrans && "Loading Transaction ..."}
+                        {transaction.length < 1 && (
+                          <li>
+                            <span className='fw-bold small text-success text-center'>
+                              No Notification
+                            </span>
+                          </li>
+                        )}
+
+                        {transaction.map((trans) => (
+                          <li key={trans._id}>
+                            <a
+                              onClick={(e) => handleQuickPin(trans)}
+                              className='dropdown-item mb-2'
+                              // href='/generate-view-qr'
+                            >
+                              <span className='fw-bold small text-success'>
+                                {/* Your Quickpin is {trans.qpin} */}
+                                {/* get service name and replace module data */}
+                                Your quickpin for your {trans.moduleData} is
+                                ready
+                              </span>
+                              <br />
+                              <span className='text-muted small'>
+                                Transaction {trans.transNumber}
+                              </span>
+                              <br />
+                              <span className='text-muted  small'>
+                                Location: {trans.locName}
+                              </span>
+                              <br />
+                              <span className='text-muted small'>
+                                Tap here to view QR
+                              </span>
+                            </a>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                  </ul>
+                  <ul className='my-auto ps-4'>
+                    {" "}
+                    <li className='nav-item dropdown'>
+                      <a
+                        href='#'
+                        role='button'
+                        data-bs-toggle='dropdown'
+                        aria-expanded='false'
+                        className='text-decoration-none text-success'
+                      >
+                        <i
+                          className='bi bi-person-circle'
+                          style={{ fontSize: "30px" }}
+                        ></i>
+                      </a>
+                      <ul className='dropdown-menu'>
+                        <li className='pe-none'>
+                          <a className='dropdown-item py-3'>
+                            <strong className='text-capitalize'>
+                              {user.firstName}
+                            </strong>
+                          </a>
+                        </li>
+                        <div className='dropdown-divider'></div>
+                        <li>
+                          <a
+                            className='dropdown-item text-center py-3'
+                            role='button'
+                            onClick={logout}
+                          >
+                            <span className='fw-bold small pe-pointer text-danger'>
+                              Logout
+                            </span>
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
                 </div>
               ) : currentUrl == "/login" ? (
                 ""

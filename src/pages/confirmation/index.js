f6832cd81523584b3923af4f1ec81ab4375b987c
ryo sakakibara <ryo.sakakibara@aiqueinnovation.com>
2023-10-19 16:54:07 +0800

    updated registration success page

--- src/pages/confirmation/index.js
+++ src/pages/confirmation/index.js
@@ -4,6 +4,8 @@ import LeftPanel from "@/components/layout/leftPanel";
 import Menu from "@/components/layout/header/menu";
 import Image from "next/image";
 import qubeeAvatar from "@/assets/img/qubeeAvatar.svg";
+import qubeeStanding from "@/assets/img/qubeeStanding.png"
+import qubeLogoBadge from "@/assets/img/qubeLogoBadge.svg"
 
 const ConfirmationPage = () => {
   return (
@@ -12,18 +14,30 @@ const ConfirmationPage = () => {
         <LeftPanel description='SMARTLOCKERS' title='Discover the power of' />
         <div className='col-12 col-md-6 right-panel'>
           <div className='row'>
-            <div className='col-lg-12 pt-5'>
-              <div className='container-fluid text-center w-100 align-self-center'>
+            <div className='col-lg-12'>
+              <div className='container-fluid text-center w-100'>
                 <div>
-                  <Image src={qubeeAvatar} className='w-100 avatar' />
-                  <span className='font-semibold'>
-                    Congratulations, your account
-                  </span>
-                  <br />
-                  <span>has been registered successfully </span>
-
-                  <Link href=''>click to redirect</Link>
+                  <Image src={qubeeStanding} className="qubee-standing h-100"/>
                 </div>
+                
+                <div className="h">
+                  <Image src={qubeLogoBadge} className="qubeLogoBadge"/>
+                </div>
+
+                <div className="">
+                  <span className='font-semibold'>
+                    <h5 className="font-success">Registration successful!</h5>
+                  </span>
+                  <span className="text-muted text-loader">Redirecting you to login... </span>
+                </div>
+
+                <div className="mt-5">
+                  <p className="text-muted w-100 mb-0">Taking too long?</p>
+                  <Link href='/login'>
+                    <small>Login</small>
+                  </Link>
+                </div>
+
               </div>
             </div>
           </div>
